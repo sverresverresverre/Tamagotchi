be1e4e2b104b9189efc9a7adc225856d7410482f
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2024-09-27 13:18:59 +0200

    shalala

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Tamagotchi!");
+
+Tamagotchi myTama = new Tamagotchi();
+
+Console.WriteLine("Please choose a name for your Tamagotchi.");
+myTama.name = Console.ReadLine();
+
+Console.WriteLine($"Whoa... {myTama.name} is a crazy name!!!");
+
+while (myTama.GetAlive() == true)
+{
+    Console.Clear();
+    myTama.PrintStats();
+    Console.WriteLine("Now what do you want to do?");
+    Console.WriteLine($"1. Teach {myTama.name} a new word");
+    Console.WriteLine($"2. Talk to {myTama.name}");
+    Console.WriteLine($"3. Feed {myTama.name}");
+    Console.WriteLine($"4. Do nothing");
+
+    string doWhat = Console.ReadLine();
+    if (doWhat == "1")
+    {
+        Console.WriteLine("What word?");
+        string word = Console.ReadLine();
+        myTama.Teach(word);
+    }
+    if (doWhat == "2")
+    {
+        myTama.Hi();
+    }
+    if (doWhat == "3")
+    {
+        myTama.Feed();
+    }
+    else
+    {
+        Console.WriteLine("Doing nothing...");
+    }
+
+    myTama.Tick();
+    Console.WriteLine("Press [ENTER] to continue...");
+    Console.ReadLine();
+}
+
+Console.WriteLine($"WHAT THE SHALALA?!? {myTama.name} is dead! ):");
+Console.WriteLine("Press [ENTER] to quit");
+Console.ReadLine();
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,70 @@
+public class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private List<string> words = new List<string>() {"Shalala"};
+    private bool isAlive;
+    public string name;
+
+    public Tamagotchi()
+    {
+        isAlive = true;
+    }
+
+    public void Feed()
+    {
+        Console.WriteLine($" [{name}] eats and becomes less hungry");
+        hunger -= 2;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
+    }
+
+    public void Hi()
+    {
+        int wordNum = Random.Shared.Next(words.Count);
+        Console.WriteLine($" [{name}] says: {words[wordNum]}");
+        ReduceBoredom();
+    }
+
+    public void Teach(string word)
+    {
+        Console.WriteLine($" [{name}] learns: {word}");
+        words.Add(word);
+        ReduceBoredom();
+    }
+
+    public void Tick()
+    {
+        hunger++;
+        boredom++;
+        if (hunger > 10 || boredom > 10)
+        {
+            isAlive = false;
+        }
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Vocabulary: {words.Count} words");
+    }
+
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
+
+    public void ReduceBoredom()
+    {
+        Console.WriteLine($" [{name}] is now less bored!");
+
+        boredom -= 2;
+        if (boredom < 0)
+        {
+            boredom = 0;
+        }
+  }
+
+}
+
